e5ce608b359985911732dad77efcd69afb21415d
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-05 22:36:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,11 @@
 def my_collect(array)
-  
+  new_array = []
   i = 0 
   while i < array.length 
-     yield array[i]
+     new_array.push(yield array[i])
      i += 1 
-    
- 
-  end
- 
+    end
+ new_array
 end
 
 
