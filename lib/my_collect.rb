646da6da87f91445eb09ddddb40d656a08de4657
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-05 21:58:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,1 @@
 def my_collect(array)
-  i = 0
-  new_array = []
-  while i < array.length 
-    yield 
-    i += 1 
-  end
- new_array
-end 
-
-my_collect(languages) do |lang|
- new_array.push(lang.upscase)
-end
