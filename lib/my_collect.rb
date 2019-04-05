9aec92fa5112d6d92628a99c40b1d90fd68e4981
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-05 22:35:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -12,6 +12,6 @@ end
 
 
 
-my_collect(["ruby", "javascript", "python", "objective-c"]) do |lang|
- lang.upcase
-end
+#my_collect(["ruby", "javascript", "python", "objective-c"]) do |lang|
+# lang.upcase
+#end
