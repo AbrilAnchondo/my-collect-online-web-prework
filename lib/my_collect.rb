e879ab37395f64cb5ccdb93fa68b683588b54b72
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-05 21:48:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,13 @@
 def my_collect(array)
   i = 0
   while i < array.length 
-  yield 
-  i += 1 
-  
+    yield 
+    i += 1 
+  end
+  array 
 end 
 
+my_collect() do |item|
+  item.capitalize 
+
+end
